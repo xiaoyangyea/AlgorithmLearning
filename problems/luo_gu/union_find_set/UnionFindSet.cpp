3f037e82cd78c254bc6f504a5b9c990498a9b695
Zechuan Han <1127153422@qq.com>
2026-04-10 22:03:05 +0800

    feat: add union find set problem

---
+++ problems/luo_gu/union_find_set/UnionFindSet.cpp
@@ -0,0 +1,45 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+typedef pair<int, int> PII;
+int findRoot(int x, vector<PII>& root) {
+    if(x != root[x].first)
+        root[x].first = findRoot(root[x].first, root);
+    return root[x].first;
+}
+
+void unionSet(int x, int y, vector<PII>& root) {
+    int _x = findRoot(x, root);
+    int _y = findRoot(y, root);
+    if(_x == _y) return;
+    if(root[_x].second < root[_y].second) root[_x].first = _y;
+    else if(root[_x].second > root[_y].second) root[_y].first = _x;
+    else {
+        root[_y].first = _x;
+        root[_x].second ++;
+    }
+}
+
+bool isSameSet(int x, int y, vector<PII>& root) {
+    int _x = findRoot(x, root);
+    int _y = findRoot(y, root);
+    return _x == _y;
+}
+
+void initSet(vector<PII>& root) {
+    for(int i = 0; i < root.size(); i++)
+        root[i].first = i, root[i].second = 0;
+}
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+    vector<PII>root(n + 1);
+    initSet(root);
+    while(m --) {
+        int x, y, z;
+        cin>>z>>x>>y;
+        if(z == 1) unionSet(x, y, root);
+        else if(z == 2) cout<<(isSameSet(x, y, root) ? 'Y':'N')<<endl;
+    }
+}
